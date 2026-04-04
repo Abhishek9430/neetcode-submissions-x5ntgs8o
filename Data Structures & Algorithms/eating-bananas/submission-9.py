a14e3b0397bfdd96a61f062525b84159282a1df3
Abhishek9430 <92214272+Abhishek9430@users.noreply.github.com>
2026-04-05 04:06:14 +0530

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def calc_time(rate):
+            time = 0
+            for pile in piles:
+                time+=math.ceil(pile/rate)
+            return time
+
+
+        i = 1 # min rate
+        j = max(piles) # max rate possible
+
+        # choosing between rates
+        min_rate = float("inf")
+        while i <= j:
+            rate = i + (j-i)//2
+            time_taken = calc_time(rate)
+            if time_taken <= h: # less than target still can do better
+                min_rate = min(rate,min_rate)
+                j = rate - 1
+            else:
+                i = rate + 1 # crossing the time limit hence need to reduce rate
+            
+        return min_rate
+
